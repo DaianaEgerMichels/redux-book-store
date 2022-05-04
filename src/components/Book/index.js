09e4b14e81bab5e3cf180595d4cbf49f53ced492
Daiana Michels <daianaem.12@gmail.com>
2022-05-04 11:14:18 -0300

    adiciona rotas aos botões

--- src/components/Book/index.js
+++ src/components/Book/index.js
@@ -1,16 +1,20 @@
 import React from "react";
 import details from './images/details.png';
-import cart from './images/cart.png'
+import cart from './images/cart.png';
+import { useNavigate} from 'react-router-dom';
 
-function Book({item}){
+function Book({item, addToCart}){
+
+  const navigate = useNavigate();
+
     return(
         <div key={item.id} className='book-item'>
           <img width="150" src={item.image} alt={item.title} />
           <span className='book-item-title'>{item.title}</span>
           <span className='book-item-price'>{item.price}</span>
           
-          <button className='book-button'><img alt={"details"} src={details} width="16px" height={"16px"}></img>Detalhes</button>
-          <button className='book-button'><img alt={"cart"} src={cart} width="16px" height={"16px"}></img>Comprar</button>
+          <button className='book-button' onClick={()=> navigate('detalhes')} ><img alt={"details"} src={details} width="16px" height={"16px"}></img>Detalhes</button>
+          <button className='book-button'onClick={()=> addToCart(item)}><img alt={"cart"} src={cart} width="16px" height={"16px"}></img>Comprar</button>
         </div>
     );
 }
