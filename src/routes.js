74e196ca9c1e7523e993409eaaa9b58a9c644bdf
Daiana Michels <daianaem.12@gmail.com>
2022-05-09 15:38:11 -0300

    adiciona nova rota e persistencia de dados

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Routes as Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
 import Details from './pages/Details';
+import Checkout from './pages/Checkout';
 
 function Routes(){
     return(
@@ -10,6 +11,7 @@ function Routes(){
             <Route exact path='/' element={<Home />} />
             <Route path='/carrinho' element={<Cart />} />
             <Route path='/detalhes' element={<Details />} />
+            <Route path='/checkout' element={<Checkout />} />
         </Switch>
     );
 }
