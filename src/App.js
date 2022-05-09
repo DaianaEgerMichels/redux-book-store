24f4b937996b03f15a450fa64f447f72420dda2b
Daiana Michels <daianaem.12@gmail.com>
2022-05-09 15:39:51 -0300

    adiciona store e persistor

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import Menu from "./components/Menu";
 import Routes from "./routes";
-import store from "./store"
+import {store, persistor} from "./store";
+import { PersistGate } from 'redux-persist/integration/react';
 
 // 1- Configurar o componente de Provider no App.js
 // 2- Configurar a store da aplicação (uma série de passos)
@@ -16,10 +17,12 @@ function App() {
   
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <Menu/>
-        <Routes/>
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <Menu/>
+          <Routes/>
+        </BrowserRouter>
+      </PersistGate>
   </Provider>
   );
 }
